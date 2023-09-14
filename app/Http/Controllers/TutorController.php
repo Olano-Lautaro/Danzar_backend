942ed597527cbf09b9cb3ca68a2c24c7765e24a6
Olano-Lautaro <lautarofcbgg@gmail.com>
2023-09-14 20:17:28 -0300

    CRUD tutor

--- app/Http/Controllers/TutorController.php
+++ app/Http/Controllers/TutorController.php
@@ -12,7 +12,9 @@ class TutorController extends Controller
      */
     public function index()
     {
-        //
+        $tutors = Tutor::all();
+
+        return $tutors->toJson();
     }
 
     /**
@@ -28,38 +30,66 @@ class TutorController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+            'phone_number' => 'required',
+            'dni' => 'required'  
+        ]);
+
+        $tutor = Tutor::create([
+            'name' => $request->name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'dni'=> $request->dni  
+        ]);
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Tutor $tutor)
+    public function show(string $id)
     {
-        //
+        $tutor = Tutor::where('id', $id)->get();
+
+        return $tutor->toJson();
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Tutor $tutor)
+    public function edit(string $id)
     {
-        //
+        $tutor = Tutor::where('id', $id)->get();
+
+        return $tutor->toJson();
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Tutor $tutor)
+    public function update(Request $request, String $id)
     {
-        //
+        $tutor = Tutor::find($id);
+
+        $tutor->name = $request->name;
+        $tutor->last_name = $request->last_name;
+        $tutor->email = $request->email;
+        $tutor->phone_number = $request->phone_number;
+        $tutor->dni = $request->dni;
+
+        $tutor->save();
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tutor $tutor)
+    public function destroy(string $id)
     {
-        //
+        $tutor = Tutor::destroy($id);
     }
 }
